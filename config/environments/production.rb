f826cbed1811b7bc1d75bfb6308489430decb220
Sam Stern <hatboysam@gmail.com>
2012-01-02 21:33:29 -0500

    Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,8 @@ Studyvault::Application.configure do
   config.active_support.deprecation = :notify
   
   #action mailer - for GMail
-  #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
   #:address              => "smtp.gmail.com",
   #:port                 => 587,
   #:domain               => 'studyheist.com',
@@ -57,4 +57,10 @@ Studyvault::Application.configure do
   #:password             => 'shep222222',
   #:authentication       => 'plain',
   #:enable_starttls_auto => true  }
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => 'app2241528@heroku.com',
+  :password       => 'll6hbcon',
+  :domain         => 'heroku.com'}
 end
