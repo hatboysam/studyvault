71d175fa883267894c27f2f538b52faf70134303
Sam Stern <hatboysam@gmail.com>
2012-01-05 15:00:13 -0500

    Home Page Improvements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
   
-  before_filter :check_download
+  before_filter :check_params
   
-  def check_download
+  def check_params
+    if params[:signedin]
+      if params[:signedin] == "false"
+        flash.now[:error] = "You must sign in to do that, please sign in or make an account"
+      end
+    end
+    
     if params[:downloaded]
       if params[:downloaded] == "true"
         flash.now[:success] = "Thanks for downloading, please don't forget to rate your downloads!"
